e9e33dc759e4d0badf598f4c3252682341862818
Leonardo Sanclemente <leonardo.sanclemente@koombea.com>
2014-09-23 15:10:07 -0500

    annotated gem added and models annotated ftw :D

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: statuses
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#  user_id    :integer
+#
+
 class Status < ActiveRecord::Base
 end
